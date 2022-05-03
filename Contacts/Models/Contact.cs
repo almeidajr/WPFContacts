6ecdf6706034ff35cea434772cb212762c8bfb2c
José Rogério de Almeida Júnior <Jose.Junior@andritz.com>
2022-05-03 16:21:38 -0300

    Save new contact using SQLite

---
+++ Contacts/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace Contacts.Models;
+
+public class Contact
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+}
--- Contacts/NewContactWindow.xaml.cs
+++ Contacts/NewContactWindow.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Contacts.Models;
+using SQLite;
+using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Contacts
 {
@@ -26,6 +18,19 @@ namespace Contacts
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
+            var contact = new Contact()
+            {
+                Name = NameField.Text,
+                Email = EmailField.Text,
+                Phone = PhoneField.Text,
+            };
+            var database = "contacts.db";
+            var directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(directory, database);
+            using var connection = new SQLiteConnection(path);
+            connection.CreateTable<Contact>();
+            connection.Insert(contact);
+
             Close();
         }
     }
