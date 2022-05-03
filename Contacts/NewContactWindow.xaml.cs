405d0c8c28f5d50dae4454e1bd80347f5600d3cc
José Rogério de Almeida Júnior <Jose.Junior@andritz.com>
2022-05-03 15:31:49 -0300

    Add an way to open a subwindow

--- Contacts/NewContactWindow.xaml.cs
+++ Contacts/NewContactWindow.xaml.cs
@@ -10,22 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Contacts
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewContactWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewContactWindow : Window
     {
-        public MainWindow()
+        public NewContactWindow()
         {
             InitializeComponent();
         }
 
-        private void NewContactButton_Click(object sender, RoutedEventArgs e)
-            => new NewContactWindow().ShowDialog();
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
